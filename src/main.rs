be42e08e8a9ba76ccb3692e78d0792e54bf705e6
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-17 18:53:57 +0200

    *

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+  let matches = cli::command_line();
+
+  match matches.subcommand() {
+    Some(("run", sub_matches)) => {
+      let script = sub_matches.get_one::<String>("script").unwrap();
+      run_script(script);
+    }
+    _ => {}
+  }
+}
+
+fn run_script(script: &str) {
+  let mut script_path = std::env::current_dir().unwrap();
+  script_path.push(script);
+  let script_path_str = script_path.to_str().unwrap();
+  println!("script_path_str: {}", script_path_str);
 }
