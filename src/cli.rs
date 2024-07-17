522b31798c22c0b960dcea4aa48a3b85106e552d
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-17 23:25:22 +0200

    *

--- src/cli.rs
+++ src/cli.rs
@@ -1,15 +1,13 @@
-use clap::{Arg, Command};
+use clap::{arg, Command};
 
 pub fn command_line() -> clap::ArgMatches {
   let matches = Command::new("dotenv.rs")
     .about("Rust-based, speedy, secure and versatile dotenv.")
-    .subcommand_required(true)
-    .arg_required_else_help(true)
     .author("Yazalde Filimone <yazaldefilimon@gmail.com>")
     .subcommand(
       Command::new("run")
         .about("run a script")
-        .arg(Arg::new("script").help("The script to run").required(true).index(1)),
+        .arg(arg!(<COMMAND> ... "the script to run")),
     )
     .get_matches();
 
