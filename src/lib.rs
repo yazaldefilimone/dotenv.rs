0759138601d95bbf513c0acc85250a7b06ca3728
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-18 12:02:07 +0200

    *

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
 mod commands;
+mod crypto;
 mod env;
-pub use commands::*;
-pub use env::*;
+mod storage;
+mod utils;
+
+pub fn run_in_command_line(command: Vec<String>) {
+  commands::run(&command);
+}
+
+pub fn scanner_workspace() -> Vec<(String, String)> {
+  return env::loader::load_workspace();
+}
