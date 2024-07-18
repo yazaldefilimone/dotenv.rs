7b131cb398c34087172d3f04ef1b28228407b82c
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-18 12:02:07 +0200

    *

--- src/env/mod.rs
+++ src/env/mod.rs
@@ -4,9 +4,7 @@ pub mod key_manager;
 pub mod loader;
 pub mod parser;
 pub mod saver;
-pub mod scanner;
 
 pub use key_manager::key_manager;
 pub use loader::loader;
 pub use parser::*;
-pub use scanner::scanner;
